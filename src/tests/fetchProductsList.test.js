f7a2114fda6cd55ed47a37478971ce4b742f40df
LucasArent <arentlucas2708@gmail.com>
2023-03-02 06:14:36 -0300

    requisito 1 dnv

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -3,32 +3,26 @@ import { fetchProductsList } from '../helpers/fetchFunctions';
 import computadorSearch from './mocks/search';
 
 describe('Teste a função fetchProductsList', () => {
-it('fetchProductsList é uma função', async () => {
-expect.assertions(1);
-const result = await fetchProductsList();
-expect(typeof result).toBe('function')
-});
+  it('fetchProductsList é uma função', () => {
+    expect(typeof fetchProductsList).toBe('function');
+  });
 
-it('Teste se, ao chamar a função fetchProductsList com o argumento computador', async () => {
-const searchTerm = 'computador';
-jest.spyOn(global, 'fetch');
-await fetchProductsList(searchTerm);
-expect(fetch).toHaveBeenCalled();
-expect(fetch).toHaveBeenCalledWith(https://api.mercadolibre.com/sites/MLB/search?q=${searchTerm});
-global.fetch.mockRestore();
-});
+  it('fetch é chamado ao executar fetchProductsList', async () => {
+    await fetchProductsList('computador')
+    expect(fetch).toHaveBeenCalled()
+  });
 
-it('Teste se o retorno da função fetchProductsList com o argumento computador', async () => {
-const searchTerm = 'computador';
-const expected = computadorSearch;
-jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve({ json: () => Promise.resolve(expected) }));
-const result = await fetchProductsList(searchTerm);
-expect(result).toEqual(expected);
-global.fetch.mockRestore();
-});
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
+    await fetchProductsList('computador')
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+  });
 
-it('Teste se, ao chamar a função fetchProductsList sem argumento, retorna um erro com mensagem', async () => {
-expect.assertions(1);
-await expect(fetchProductsList()).rejects.toThrow('Termo de busca não informado');
-});
+  it('Teste se o retorno da função fetchProductsList com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo.', async () => {
+    const argumentcomp = await fetchProductsList('computador');
+    expect(argumentcomp).toStrictEqual(computadorSearch);
+    });
+
+  it('Teste se, ao chamar a função fetchProductsList sem argumento, retorna um erro com a mensagem: "Termo de busca não informado"', async () => {
+    await expect(fetchProductsList()).rejects.toThrow('Termo de busca não informado');
+  });
 });
