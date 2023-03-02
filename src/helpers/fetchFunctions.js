be05e3761a96d3299ddcd15bcc378ee5da6c21cc
LucasArent <arentlucas2708@gmail.com>
2023-03-02 06:51:04 -0300

    requisito 2 ref

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -4,10 +4,10 @@ export const fetchProduct = () => {
 
 export const fetchProductsList = async (findThis) => {
   if (!findThis) {
-  throw new Error('Termo de busca não informado');
+    throw new Error('Termo de busca não informado');
   }
-  
+
   const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${findThis}`);
-  const data = await response.json();console.log(data)
+  const data = await response.json(); console.log(data);
   return data.results;
 };
