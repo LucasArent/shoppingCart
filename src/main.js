e96334939ac92358bf986bce8f426fa10070e945
LucasArent <arentlucas2708@gmail.com>
2023-03-03 10:05:38 -0300

    requisito 4 e 5 + refatoração e test de push

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import { fetchProductsList } from './helpers/fetchFunctions';
 import { createProductElement } from './helpers/shopFunctions';
 
 function displayProducts() {
+  const loaded = document.createElement('p');
+  loaded.textContent = 'carregando...';
+  loaded.classList.add('loading');
+  document.body.appendChild(loaded);
+
   fetchProductsList('computador')
     .then((products) => {
       const productsSection = document.querySelector('.products');
@@ -12,10 +17,18 @@ function displayProducts() {
         productsSection.appendChild(productElement);
       });
     })
-    .catch((error) => console.log(error.message));
+    .catch((error) => {
+      console.log(error.message);
+      const erro = document.createElement('p');
+      erro.textContent = 'Algum erro ocorreu, recarregue a página e tente novamente';
+      erro.classList.add('error');
+      document.body.appendChild(erro);
+    })
+    .finally(() => {
+      document.querySelector('.loading').remove();
+    });
 }
 
 displayProducts();
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
-console.log(displayProducts);
